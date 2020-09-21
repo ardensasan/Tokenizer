2f942a8a49e9c52f6f282b41fcb6ddf073866de1
ardensasan <52578552+ardensasan@users.noreply.github.com>
2020-09-21 16:28:48 +0800

    - removed increment decrement variable - changed constant variables to capital letters

--- Tokenizer/Token.c
+++ Tokenizer/Token.c
@@ -4,12 +4,10 @@
 void checkToken(char exp[]) {
 	const char op[] = "Operator", num[] = "Integer", ch[] = "Identifier";
 	int n = 0, i = 0, i2;
-	const char increment[] = "++";
-	const char decrement[] = "--";
-	const char operators[] = "+-*/<>=";
-	const char opEquivalent[][20] = { "Plus","Minus","Times","Divide","Less Than", "Greater Than","Equals" };
-	const char separator[] = "{}[]();";
-	const char sEquivalent[][20] = { "Open Curly Bracket","Close Curly Bracket","Open Bracket","Close Bracket","Open Parenthesis","Close Parenthesis","Semicolon" };
+	const char OPERATORS[] = "+-*/<>=";
+	const char OPEQUIVALENT[][20] = { "Plus","Minus","Times","Divide","Less Than", "Greater Than","Equals" };
+	const char SEPARATOR[] = "{}[]();";
+	const char SEQUIVALENT[][20] = { "Open Curly Bracket","Close Curly Bracket","Open Bracket","Close Bracket","Open Parenthesis","Close Parenthesis","Semicolon" };
 	for (n = 0;exp[n] != '\0';n++) {
 		if (isdigit(exp[n])) {
 			printf("\n");
@@ -35,14 +33,14 @@ void checkToken(char exp[]) {
 			n += 2;
 			printf("\n-- \t Decrement");
 		}
-		for (i = 0;operators[i] != '\0';i++) {
-			if (exp[n] == operators[i]) {
-				printf("\n%c\t%s", exp[n], opEquivalent[i]);
+		for (i = 0;OPERATORS[i] != '\0';i++) {
+			if (exp[n] == OPERATORS[i]) {
+				printf("\n%c\t%s", exp[n], OPEQUIVALENT[i]);
 			}
 		}
-		for (i = 0;separator[i] != '\0';i++) {
-			if (exp[n] == separator[i]) {
-				printf("\n%c\t%s", exp[n], sEquivalent[i]);
+		for (i = 0;SEPARATOR[i] != '\0';i++) {
+			if (exp[n] == SEPARATOR[i]) {
+				printf("\n%c\t%s", exp[n], SEQUIVALENT[i]);
 			}
 		}
 		if (exp[n] == '\0') {
